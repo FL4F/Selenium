d6d902e8eb242649334703862f13ed713ac7d8b1
FL4F <flaviojunior.junior1@gmail.com>
2022-05-06 15:33:30 -0300

    Update projeto.py

--- projeto.py
+++ projeto.py
@@ -7,4 +7,12 @@ navegador = webdriver.Chrome()
 navegador.get('https://www.google.com.br/')
 navegador.find_element(By.XPATH, '/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input').send_keys('Cotaçao Dolar', Keys.ENTER)
 
-cotação_dolar = navegador.find_element(By.XPATH,'//*[@id="knowledge-currency__updatable-data-column"]/div[1]/div[2]/span[1]').get_attribute('data-value')
+cotacao_dolar = navegador.find_element(By.XPATH,'//*[@id="knowledge-currency__updatable-data-column"]/div[1]/div[2]/span[1]').get_attribute('data-value')
+
+navegador.get('https://www.google.com.br/')
+navegador.find_element(By.XPATH, '/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input').send_keys('Cotaçao Euro', Keys.ENTER)
+
+cotacao_euro = navegador.find_element(By.XPATH, '//*[@id="knowledge-currency__updatable-data-column"]/div[1]/div[2]/span[1]').get_attribute('data-value')
+
+navegador.get('https://www.melhorcambio.com/ouro-hoje')
+cotacao_ouro = navegador.find_element(By.XPATH,'//*[@id="comercial"]').get_attribute('value')
